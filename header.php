61bce132919dfd3d72797b420f7bd8db1d44831c
to-aman <i10v3.3.6bc@gmail.com>
2022-03-12 15:33:10 +0900

    3/12の作業分

--- header.php
+++ header.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<img src="<?php header_image(); ?>">
+<header>
+	<h1>
+		<a href="<?php esc_url( home_url( '/' ) ) ?>">
+			<img src="<?php header_image(); ?>" alt="<?php esc_attr( bloginfo( 'name' ) ) ?>" width="445" height="88">
+		</a>
+	</h1>
+	<nav>
+		<?php get_search_form(); ?>
+	</nav>
+</header>
+<nav class="nav-global">
+	<?php if ( has_nav_menu( 'global' ) ) : ?>
+		<?php wp_nav_menu( array( 'theme_location' => 'global' )); ?>
+	<?php endif; ?>
+</nav>
+<?php echo do_shortcode( '[wp-structuring-markup-breadcrumb]' );
