45cbe82c07fb1803036aba53fe9893da0296379a
to-aman <i10v3.3.6bc@gmail.com>
2022-05-09 02:17:40 +0900

    5/8作業分（コピーライト入れたフッターの調整）

--- footer.php
+++ footer.php
@@ -2,22 +2,24 @@
 </div>
 <footer>
     <div class="icon-area">
+        <div>
         <a href="https://twitter.com/aman901030">
             <i class="fab fa-twitter footer-icon"></i>
             <p>@aman901030</p>
         </a>
-    </div>
-    <div class="icon-area">
+        </div>
+        <div>
         <a href="https://www.facebook.com/tomomi.aman">
             <i class="fab fa-facebook footer-icon"></i>
             <p>tomomi.aman</p>
         </a>
-    </div>
-    <div class="icon-area">
+        </div>
+        <div>
         <a href="https://www.instagram.com/aman901030/?hl=ja">
             <i class="fab fa-instagram footer-icon"></i>
             <p>aman901030</p>
         </a>
+        </div>
     </div>
     <p><small>&copy;&nbsp;<?php bloginfo( 'name' ); ?>&nbsp;All&nbsp;rights&nbsp;reserved.</small></p>
 </footer>
