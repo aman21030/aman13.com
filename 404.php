8e93bcc78fb5f2bcef4d4bf2204d613887c339a3
to-aman <i10v3.3.6bc@gmail.com>
2022-05-14 12:05:50 +0900

    5/11～5/14作業(css調整等)

--- 404.php
+++ 404.php
@@ -2,6 +2,7 @@
 
 get_header(); ?>
 
-<h1>404 not found</h1>
+<h1>ページが存在しません</h1>
+<img class="not-found" src="<?php echo get_template_directory_uri() ?>/img/404.png" alt="404 Not Found" />
 
 <?php get_footer();
--- search.php
+++ search.php
@@ -1,28 +1,34 @@
 <?php
 
 get_header();
-
-echo get_search_query();
+?>
+<div class="home-main">
+<?php
+echo '<label class="category">', get_search_query(), '</label>';
 
 if ( have_posts() ) :
-    echo '<ul>' , PHP_EOL;
+    echo 'の検索の結果<ul>' , PHP_EOL;
     while ( have_posts() ) : the_post(); ?>
         <li>
-        <time><?php the_time( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ) ); ?></time>
-        <?php the_title( printf( '<h1><a href="%s">', get_permalink() ), '</a></h1>' ); ?>
-        <?php the_category( ',' ); ?>
-        <?php the_post_thumbnail( 'full' ); ?>
+        <div class="label"><?php the_category( ' ' ); ?></div>
+        <div class="article">
+            <?php the_post_thumbnail( 'full', printf( '<a href="%s">', get_permalink() ), '</a>' ); ?>
+            <time><?php the_time( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ) ); ?></time>
+            <?php the_title( printf( '<a href="%s"><h2>', get_permalink() ), '</h2></a>' ); ?>
+        </div>
         </li>
     <?php endwhile;
     echo '</ul>' , PHP_EOL;
 else :
-    echo '見つかりませんでした。。。';
+    echo '<br>に関する記事は見つかりませんでした。。。';
 endif;
-
+?>
+</div>
+<?php
 the_posts_pagination( array(
 	'before_page_number' => '',
-	'next_text'          => 'Next',
-	'prev_text'          => 'Prev',
+	'next_text'          => '>',
+	'prev_text'          => '<',
 	'show_all'           => __return_true(),
 	'screen_reader_text' => 'Post Navigation'
 ) );
