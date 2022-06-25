3fb4852079dc8bb3f616416248901e21d37afafb
to-aman <i10v3.3.6bc@gmail.com>
2022-06-25 15:25:48 +0900

    レスポンシブ対応途中

--- single.php
+++ single.php
@@ -3,21 +3,14 @@
 get_header();
 
 if ( have_posts() ) : ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <article class="single-article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php the_title( '<h1>', '</h1>' ); ?>
-    投稿日<time><?php the_time( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ) ); ?></time>
-    更新日<time><?php the_modified_date( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ) ); ?></time>
+    <div class="article-time">投稿日<time><?php the_time( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ) ); ?></time></div>
+    <div class="article-time">更新日<time><?php the_modified_date( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ) ); ?></time></div>
     <p><?php the_author(); ?></p>
     <?php the_content(); ?>
     </article>
-<?php endif;
-
-echo '<div class="pagenation">';
-previous_post_link();
-echo '<span class="pagenation-space"></span>';
-next_post_link();
-echo '</div>';
-?>
+<?php endif; ?>
 
 <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
 	<?php echo get_avatar( get_the_author_meta( 'user_email' ), 60, '', esc_html( get_the_author() ) ); ?>
@@ -25,10 +18,20 @@ echo '</div>';
 <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
 	<?php esc_html( the_author() ); ?>
 </a>
-<?php esc_html( the_author_meta( 'description' ) );
+<?php esc_html( the_author_meta( 'description' ) ); ?>
 
-if ( comments_open() || get_comments_number() ) {
-	comments_template();
-}
+<div class="comments">
+    <?php
+        if ( comments_open() || get_comments_number() ) {
+	    comments_template();
+        }
+    ?>
+</div>
+<?php 
+echo '<div class="pagenation">';
+previous_post_link();
+echo '<span class="pagenation-space"></span>';
+next_post_link();
+echo '</div>';
 
 get_footer();
